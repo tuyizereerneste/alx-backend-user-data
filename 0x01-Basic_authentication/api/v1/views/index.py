db90ebc165392347a2c342178c20afee75986977
tuyizereerneste <tuyizereernest@yahoo.com>
2024-04-16 13:56:47 +0200

    handle 401

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorised() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+        the status if an API
+    """
+    abort(401)
